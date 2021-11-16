eb7d12257509b5b8466b9b9f74ab28e313c23e9b
Luke Nonas-Hunter <lukenonashunter@gmail.com>
2021-11-16 01:29:16 -0500

    Replace setup.cfg with setup.py for use with developement mode

--- setup-python-venv.sh
+++ setup-python-venv.sh
@@ -6,20 +6,18 @@ echo "#####Setup Python Environment#####"
 echo "Searching for $VENV python virtual environment..."
 if [ -d "$FOLDER" ]; then
     echo "Found $FOLDER. Attempting to activate..."
-    source "$FOLDER/bin/activate"
+    source $FOLDER/bin/activate
     echo "Virtual environment activated."
-    echo "Installing symlinks to packages in python-src..."
-    ls "$PWD/python-src/" | grep -v egg-info$ | pip install -e
 else
     echo "Virtual environment not found! Attempting to create a new one..."
     [ ! -d "$PWD/.venv" ] && mkdir "$PWD/.venv"
     python3 -m venv $FOLDER
-    source "$FOLDER/bin/activate"
+    source $FOLDER/bin/activate
     echo "Virtual environment activated."
-    echo "Installing requirements..."
-    python -m pip install -r requirements.txt
-    echo "Installing symlinks to packages in python-src..."
-    ls "$PWD/python-src/" | grep -v egg-info$ | pip install -e
 fi
+echo "Installing requirements..."
+python -m pip install -r requirements.txt
+echo "Installing symlinks to packages in python-src..."
+pip install -e $PWD
 echo "#####DONE#####"
 
