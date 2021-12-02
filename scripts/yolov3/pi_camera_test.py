9d77df56a67330a5510c63c94e9474180d4ad0aa
annacommers <68507570+annacommers@users.noreply.github.com>
2021-12-02 10:16:08 -0500

    Update pi_camera_test.py

--- scripts/yolov3/pi_camera_test.py
+++ scripts/yolov3/pi_camera_test.py
@@ -26,13 +26,14 @@ from time import sleep
 from picamera import PiCamera
 from PIL import Image
 
+# Create the in-memory stream
+stream = BytesIO()
+camera = PiCamera()
+camera.start_preview()
+sleep(2)
+camera.capture(stream, format='jpeg')
+
 while True:
-    # Create the in-memory stream
-    stream = BytesIO()
-    camera = PiCamera()
-    camera.start_preview()
-    sleep(2)
-    camera.capture(stream, format='jpeg')
     # "Rewind" the stream to the beginning so we can read its content
     # stream.seek(0)
     image = Image.open(stream)
