3922f409ddc6b6809673559ca5e4578c71158623
annacommers <68507570+annacommers@users.noreply.github.com>
2021-12-02 09:27:28 -0500

    Update pi_camera_test.py

--- scripts/yolov3/pi_camera_test.py
+++ scripts/yolov3/pi_camera_test.py
@@ -31,9 +31,9 @@ stream = BytesIO()
 camera = PiCamera()
 camera.start_preview()
 sleep(2)
-camera.capture(stream, format='jpeg')
 
 while True:
+    camera.capture(stream, format='jpeg')
     # "Rewind" the stream to the beginning so we can read its content
     # stream.seek(0)
     image = Image.open(stream)
