975205d1340754d6bdf1ff411275328e4ed30f42
Luke Nonas-Hunter <lukenonashunter@gmail.com>
2021-12-06 13:05:32 -0500

    Create sample python module in class

---
+++ python-src/math_operations.py
@@ -0,0 +1,18 @@
+class MathOperations:
+    def __init__(self, x, y):
+        """Instantiate MathOperations object with two numbers to operate on.
+
+        Parameters:
+            x (int): first number.
+            y (int): second number.
+        """
+        self.x = x
+        self.y = y
+
+    def add(self):
+        """Add x and y together. X and y have been defined in the init.
+
+        Returns:
+            (int): x plus y.
+        """
+        return x+y
